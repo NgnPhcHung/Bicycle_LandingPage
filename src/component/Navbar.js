e4a5967bf7e092da561863a1d6acc64a50410dbd
nguyen phuc hung <nphbo1998@gmail.com>
2024-06-05 17:07:06 +0700

    fix: optimize code

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import Logo from "./Logo";
 import ghostImg from "../asset/ghost.png";
+import Logo from "./Logo";
 
 const Nav = styled.nav`
   width: 100%;
@@ -34,41 +34,6 @@ const Ghost = styled.img`
 
   width: var(--fontxxl);
   transform: scaleX(-1);
-`;
-const NavBlock = styled.ul`
-  list-style: none;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 20px;
-`;
-const Text = styled.p`
-  color: var(--white);
-  font-size: var(--fontmd);
-  position: relative;
-  cursor: pointer;
-
-  transition: transform 0.2 ease;
-
-  &::before {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 4px;
-    border-radius: 4px;
-    background-color: var(--greyLight);
-    bottom: 0;
-    left: 0;
-    transform-origin: right;
-    transform: scaleX(0);
-    transition: transform 0.3s ease-in-out;
-  }
-
-  &:hover::before {
-    transform-origin: left;
-    transform: scaleX(1);
-  }
 `;
 
 const Navbar = () => {
--- src/section/HeroSection.js
+++ src/section/HeroSection.js
@@ -1,9 +1,11 @@
-import gsap from 'gsap'
-import ScrollTrigger from 'gsap/ScrollTrigger'
-import React, { useLayoutEffect, useRef } from 'react'
-import styled from 'styled-components'
-import StaticBike from '../component/Bike/Hero/StaticBike'
-gsap.registerPlugin(ScrollTrigger)
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import React, { lazy, Suspense, useLayoutEffect, useRef } from "react";
+import styled from "styled-components";
+
+const StaticBike = lazy(() => import("../component/Bike/Hero/StaticBike"));
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Section = styled.section`
   width: 100vw;
@@ -18,18 +20,18 @@ const Section = styled.section`
 
   text-transform: capitalize;
 
-  &:nth-child(2){
+  &:nth-child(2) {
     right: 0;
   }
-`
-const FrontText= styled.h1`
+`;
+const FrontText = styled.h1`
   width: 66%;
   color: var(--white);
   font-size: var(--fontBig);
   text-align: left;
-  
+
   z-index: 2;
-  span{
+  span {
     margin-left: 15px;
     color: var(--green);
     width: fit-content;
@@ -44,15 +46,16 @@ const FrontText= styled.h1`
   @media (max-width: 48em) {
     font-size: var(--fontlg);
   }
-`
-const BackText= styled.span`
+`;
+const BackText = styled.span`
   align-self: flex-end;
   width: fit-content;
   z-index: 1;
   font-size: var(--fontBig);
   transform: rotate(-10deg);
   stroke-width: 1px;
-  text-shadow: -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff, 1px 1px 0 #fff;
+  text-shadow: -1px -1px 0 #fff, 1px -1px 0 #fff, -1px 1px 0 #fff,
+    1px 1px 0 #fff;
 
   @media (max-width: 70em) {
     font-size: var(--fontxxxl);
@@ -64,14 +67,14 @@ const BackText= styled.span`
     font-size: var(--fontxl);
     z-index: 2;
   }
-`
+`;
 const ModelText = styled.div`
   position: absolute;
   left: 5%;
   color: var(--white);
   font-weight: 100;
 
-  h2{
+  h2 {
     font-weight: 600;
     font-size: var(--fontxxl);
     background-image: linear-gradient(150deg, var(--gradient));
@@ -80,17 +83,17 @@ const ModelText = styled.div`
     -webkit-text-fill-color: transparent;
   }
 
-  @media (max-width: 64em){
+  @media (max-width: 64em) {
     left: unset;
     right: 5%;
     top: 0;
   }
-`
+`;
 
 const HeroSection = () => {
   const container = useRef(null);
-  const textOne = useRef(null)
-  const textTwo = useRef(null)
+  const textOne = useRef(null);
+  const textTwo = useRef(null);
 
   useLayoutEffect(() => {
     let tl = gsap
@@ -103,7 +106,7 @@ const HeroSection = () => {
         },
       })
       .fromTo(textOne.current, { x: "10%" }, { x: "-43%" }, "key1")
-      .fromTo(textTwo.current, { x: 0 }, { x: "16%" }, "key1")
+      .fromTo(textTwo.current, { x: 0 }, { x: "16%" }, "key1");
 
     return () => {
       if (tl) tl.kill();
@@ -111,16 +114,20 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <Section ref={container} className='panel' id="herosection" >
-      <FrontText ref={textTwo} >Ride your <span>bicyle</span></FrontText>
-      <BackText ref={textOne} >To heaven with us</BackText>
+    <Section ref={container} className="panel" id="herosection">
+      <FrontText ref={textTwo}>
+        Ride your <span>bicycle</span>
+      </FrontText>
+      <BackText ref={textOne}>To heaven with us</BackText>
       <ModelText>
         <h2>XCT</h2>
         Bike of ghost
       </ModelText>
-      <StaticBike/>
+      <Suspense >
+        <StaticBike />
+      </Suspense>
     </Section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
